20c47d6ad90452abcf0c90f7c60462d0e6bdb4f6
sblendorio <sblendorio@gmail.com>
2019-10-23 23:49:41 +0200

    Replacing org.reflections with guava. Minumum JDK required is now 1.8

--- src/main/java/eu/sblendorio/reflectionsspike/MainClass.java
+++ src/main/java/eu/sblendorio/reflectionsspike/MainClass.java
@@ -1,15 +1,19 @@
 package eu.sblendorio.reflectionsspike;
 
-import org.reflections.Reflections;
+import com.google.common.reflect.ClassPath;
 
+import java.io.IOException;
 import java.util.Set;
 
 public class MainClass {
 
-    public static void main(String s[]) {
-        Set<Class<? extends Base>> classes = new Reflections("").getSubTypesOf(Base.class);
-        for (Class<? extends Base> clazz: classes) {
-            System.out.println("* " + clazz.getName());
+    public static void main(String s[]) throws IOException {
+        ClassLoader cl = MainClass.class.getClassLoader();
+        Set<ClassPath.ClassInfo> classesInPackage = ClassPath.from(cl).getTopLevelClasses();
+        for (ClassPath.ClassInfo c: classesInPackage) {
+            Class cz = c.load();
+            if (Base.class.isAssignableFrom(cz))
+            System.out.println("* " + c.getName());
         }
     }
 }
